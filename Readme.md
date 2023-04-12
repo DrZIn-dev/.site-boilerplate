49e4a688cc5ead9f0f97db7e9a0fe0ae3fb18464
Kawakami Toshio <low.wing3501@fastmail.com>
2023-04-12 15:07:33 +0700

    chore: init

---
+++ Readme.md
@@ -0,0 +1,43 @@
+# About This Repo
+
+is a boilerplate for setup new site machine.
+
+# Getting Start
+
+1. Change directory to root
+
+   ```shell
+   cd ~
+   ```
+
+2. Clone this reposity and change name to .site
+
+   ```shell
+   git clone https://github.com/DrZIn-dev/.site-boilerplate.git .site
+   ```
+
+3. Copy .env.example to .env
+
+   ```shell
+   cp .env.example .env
+   ```
+
+4. Change directory to .site
+
+   ```shell
+   cd .site
+   ```
+
+5. Run provision.sh
+
+   - install docker
+   - install docker-compose
+
+   ```shell
+   bash provision.sh
+   ```
+
+6. Setup public container
+   ```shell
+   bash setup-public.sh
+   ```
---
+++ cmd/provision.sh
@@ -0,0 +1,33 @@
+#!bin/bash
+
+# Check docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin is installed
+if ! command -v docker &> /dev/null
+then
+  echo "        Started Installing Docker"
+  sudo apt-get update -y
+  sudo apt-get install \
+      ca-certificates \
+      curl \
+      gnupg -y
+
+  sudo mkdir -m 0755 -p /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+  echo \
+    "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    
+  sudo apt-get update -y
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+  echo "[ OK  ] Finished Installing Docker"
+else
+  echo "[ SKIPPED  ] Docker is already installed"
+fi
+
+# sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# sudo chmod +x /usr/local/bin/docker-compose
+
+# docker swarm init
+
+# bash ./setup-public.sh
