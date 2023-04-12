7a083646559b0f8b26a3c72afbede7e3a59f103d
Kawakami Toshio <low.wing3501@fastmail.com>
2023-04-12 15:26:29 +0700

    update: text color

--- cmd/provision.sh
+++ cmd/provision.sh
@@ -1,14 +1,44 @@
 #!bin/bash
 
 # echo [ OK  ] with green color in ok text
-# echo -e "\e[32m[ OK  ]\e[0m"
+echo -e "\e[32m[ OK  ]\e[0m"
 # echo [ SKIPPED  ] with yellow color in skipped text
-# echo -e "\e[33m[ SKIPPED  ]\e[0m"
-# hide all outout from command
+echo -e "\e[33m[ SKIPPED  ]\e[0m"
+# echo [ FAILED  ] with red color in failed text
+echo -e "\e[31m[ FAILED  ]\e[0m"
+# echo "tab" with 8 spaces in info text
+echo "        "
+
+# Function to echo [ OK  ] with green color in ok text
+ok() {
+  echo -e "\e[32m[ OK  ]\e[0m $1"
+}
+
+# Function to echo [ SKIPPED  ] with yellow color in skipped text
+skipped() {
+  echo -e "\e[33m[ SKIPPED  ]\e[0m $1"
+}
+
+# Function to echo [ FAILED  ] with red color in failed text
+failed() {
+  echo -e "\e[31m[ FAILED  ]\e[0m $1"
+}
+
+# Function to echo "tab" with 8 spaces
+info() {
+  echo "        $1"
+}
+
+# Check if user is root
+if [ "$EUID" -ne 0 ]
+  then failed "Please run as root"
+  exit
+fi
+
 # Check docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin is installed
+info "Started Installing Docker"
 if ! command -v docker &> /dev/null
 then
-  echo "        Started Installing Docker"
   sudo apt-get update -y
   sudo apt-get install \
       ca-certificates \
@@ -25,24 +55,36 @@ then
     
   sudo apt-get update -y
   sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-  echo -e "\e[32m[ OK  ]\e[0m Finished Installing Docker"
+  # echo -e "\e[32m[ OK  ]\e[0m Finished Installing Docker"
+  ok "Finished Installing Docker"
 else
-  echo -e "\e[33m[ SKIPPED  ]\e[0m Docker is already installed"
+  # echo -e "\e[33m[ SKIPPED  ]\e[0m Docker is already installed"
+  skipped "Docker is already installed"
 fi
 
 # Check docker swarm is initialized
+# echo "        Started Initializing Docker Swarm"
+info "Started Initializing Docker Swarm"
 if ! docker info | grep -q "Swarm: active"
 then
-  echo "        Started Initializing Docker Swarm"
   docker swarm init
-  echo -e "\e[32m[ OK  ]\e[0m Finished Initializing Docker Swarm"
+  # echo -e "\e[32m[ OK  ]\e[0m Finished Initializing Docker Swarm"
+  ok "Finished Initializing Docker Swarm"
 else
-  echo -e "\e[33m[ SKIPPED  ]\e[0m Docker Swarm is already initialized"
+  # echo -e "\e[33m[ SKIPPED  ]\e[0m Docker Swarm is already initialized"
+  skipped "Docker Swarm is already initialized"
 fi
 
-# sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-# sudo chmod +x /usr/local/bin/docker-compose
-
-# docker swarm init
-
-# bash ./setup-public.sh
+# Check docker-compose is installed
+# echo "        Started Installing Docker-Compose"
+info "Started Installing Docker-Compose"
+if ! command -v docker-compose &> /dev/null
+then
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+  # echo -e "\e[32m[ OK  ]\e[0m Finished Installing Docker-Compose"
+  ok "Finished Installing Docker-Compose"
+else
+  # echo -e "\e[33m[ SKIPPED  ]\e[0m Docker-Compose is already installed"
+  skipped "Docker-Compose is already installed"
+fi
